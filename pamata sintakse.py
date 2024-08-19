e70a5329621342997ac1d05a238684327e72deba
TeachDidzis <155529790+TeachDidzis@users.noreply.github.com>
2024-08-19 12:34:52 +0300

    Izvades noformēšana piemērs

--- pamata sintakse.py
+++ pamata sintakse.py
@@ -8,7 +8,7 @@ print("Hello World")
 sk1 = input("ievadiet skaitli, kuru skaitīsiet:")
 sk2 = input("ievadiet skaitli, kuru skaitīsiet:")
 summa = int(sk1) + int(sk2)
-print(summa)
+print(f"ievadīto skatļu {sk1} un {sk2} summa ir {summa}")
 
 teksts = "Hello"
 for i in teksts:
